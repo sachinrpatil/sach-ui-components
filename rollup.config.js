ed8493415c40e608f34a370c443ac7c830794263
sachinrpatil <patil.sachin24@gmail.com>
2022-06-12 18:50:02 +0530

    peer dependencies and optimization

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import postcss from "rollup-plugin-postcss";
 const packageJson = require("./package.json");
@@ -22,10 +24,12 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      postcss()
+      postcss(),
+      terser(),
     ],
   },
   {
